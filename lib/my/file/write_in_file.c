8f0f71cc59689e177cb1d1dd983dda3de66149f7
arthur <arthur.teisseire@epitech.eu>
2018-03-29 14:30:33 +0200

    [*]write_in_file -- now creat file if not exists woth correct rights

--- lib/my/file/write_in_file.c
+++ lib/my/file/write_in_file.c
@@ -11,13 +11,18 @@
 
 int write_in_file(int fd_in, char *pathname, int flag_open)
 {
-	int fd = open(pathname, flag_open);
 	char buff[READ_SIZE];
-	int size = read(fd_in, buff, READ_SIZE);
+	int size;
+	int fd = open(pathname, O_WRONLY);
 
+	if (fd == -1)
+		fd = open(pathname, flag_open,
+		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	size = read(fd_in, buff, READ_SIZE);
 	while (size != 0) {
 		write(fd, buff, size);
 		size = read(fd_in, buff, READ_SIZE);
-	}   
+	}
+	close(fd);
 	return (0);
 }
