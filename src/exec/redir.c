4eedd5fed4faa23f2cda16dfcf3a38c8212dea08
arthur <arthur.teisseire@epitech.eu>
2018-04-09 13:24:33 +0200

    [*]redir_left -- it's now working as expected

--- src/exec/redir.c
+++ src/exec/redir.c
@@ -20,6 +20,7 @@ int redir1_right(btree_t *root)
 		execout_to_pipe(cmd_left);
 	cmd->pipefd[0] = cmd_left->pipefd[0];
 	write_in_file(cmd_right->str, cmd->pipefd[0], O_TRUNC);
+	cmd->pipefd[0] = cmd_left->pipefd[0];
 	return (0);
 }
 
@@ -33,18 +34,21 @@ int redir2_right(btree_t *root)
 		execout_to_pipe(cmd_left);
 	cmd->pipefd[0] = cmd_left->pipefd[0];
 	write_in_file(cmd_right->str, cmd->pipefd[0], O_APPEND);
+	cmd->pipefd[0] = cmd_left->pipefd[0];
 	return (0);
 }
 
 int redir1_left(btree_t *root)
 {
 	cmd_t *cmd = root->item;
+	cmd_t *cmd_left = root->left->item;
 	cmd_t *cmd_right = root->right->item;
 	int fd = open(cmd_right->str, O_RDONLY);
 
 	if (fd < 0)
 		return (fd);
-	my_fdcpy(cmd->pipefd[0], fd);
-	execout_to_pipe(root->item);
+	cmd_left->pipefd[0] = fd;
+	execout_to_pipe(cmd_left);
+	cmd->pipefd[0] = cmd_left->pipefd[0];
 	return (0);
 }
